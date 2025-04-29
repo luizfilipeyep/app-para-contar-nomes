0bc85b74e6812bcc6ec69664946e36d29ff9ec21
luizfilipeyep <83364189+luizfilipeyep@users.noreply.github.com>
2025-04-29 18:58:58 -0300

    button and form

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
+import { useState } from 'react'
 
 export default function App() {
+  const [nome, setNome] = useState("Luiz")
+  const [input, setInput] = useState("")
+
+  const alterarNome = () => {
+    setNome(input)
+    setInput("")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o novo nome"
+        value={input}
+        onChangeText={(texto) => setInput(texto)}
+      ></TextInput>
+
+      <TouchableOpacity 
+        style={styles.btn}
+        onPress={alterarNome} 
+      >
+        <Text style={styles.btnText}>Alterar Nome</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.titulo}>{nome}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    backgroundColor: "#22272e"
   },
+  titulo: {
+    fontSize: 90,
+    color: "#4682B4"
+  },
+  btn: {
+    backgroundColor: "#FFA500",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50
+  },
+  btnText: {
+    color: "#ffffff",
+    fontSize: 22
+  },
+  input: {
+    backgroundColor: "#ffffff",
+    height: 50
+  }
 });
